e1040237e5fc4e886837bc301073c22c768e43df
JohnMorris4 <johnmorris@morrisje.com>
2018-05-24 06:01:17 -0400

    Creating the Desktop Contacts Application

---
+++ DesktopContacts/Models/Contacts.cs
@@ -0,0 +1,22 @@
+﻿using SQLite;
+
+namespace DesktopContacts.Models
+{
+    class Contacts
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+
+        [MaxLength(50)]
+        public string Name { get; set; }
+
+        [MaxLength(75), Unique]
+        public string Email { get; set; }
+
+
+        public string PhoneNumber { get; set; }
+
+
+
+    }
+}
---
+++ DesktopContacts/NewContactWindow.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+
+namespace DesktopContacts
+{
+    /// <summary>
+    /// Interaction logic for NewContactWindow.xaml
+    /// </summary>
+    public partial class NewContactWindow : Window
+    {
+        public NewContactWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void SaveButton_OnClick(object sender, RoutedEventArgs e)
+        {
+
+            //TODO - Setup the Save Contact
+            Close();
+        }
+    }
+}
